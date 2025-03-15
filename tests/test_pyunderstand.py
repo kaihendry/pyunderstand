8a1f6cdaa7d134e258666aa7a228b69ae0a2bbc1
Kai Hendry <hendry@iki.fi>
2025-03-15 07:35:55 +0000

    fix structure

--- tests/test_pyunderstand.py
+++ tests/test_pyunderstand.py
@@ -1,9 +1,5 @@
 import pytest
-from pyunderstand import foo, __version__
+from pyunderstand import foo
 
 def test_foo_returns_bar():
     assert foo() == "bar"
-
-def test_version_exists():
-    assert isinstance(__version__, str)
-    assert len(__version__) > 0 
